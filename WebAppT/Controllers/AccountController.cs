286d4f556851deae43bdf4b2b15aecc2a066d4f3
Ömer <omareany99@hotmail.com>
2024-03-24 06:14:49 +0100

    account is completed

--- WebAppT/Controllers/AccountController.cs
+++ WebAppT/Controllers/AccountController.cs
@@ -1,8 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppT.ViewModels;
 
 namespace WebAppT.Controllers;
 
 public class AccountController : Controller
 {
- 
+    [Route("/profile")]
+    public IActionResult Details(AccountDetailsViewModel viewModel)
+    {
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccountDetailsViewModel viewModel)
+    {
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult AddressInfo(AccountDetailsViewModel viewModel)
+    {
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
 }
---
+++ WebAppT/Models/AccountDetailsBasicInfoModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppT.Models;
+
+public class AccountDetailsBasicInfoModel
+{
+    [DataType(DataType.ImageUrl)]
+    public string? ProfileImg {  get; set; }
+
+    [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "First name is required")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name", Prompt = "Enter your Last name", Order = 1)]
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; } = null!;
+
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email address", Prompt = "Enter your email", Order = 2)]
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "PhoneNumber", Prompt = "Enter your phone", Order = 3)]
+    [Required(ErrorMessage = "Phone is required")]
+    public string PhoneNumber { get; set; } = null!;
+
+    [DataType(DataType.MultilineText)]
+    [Display(Name = "Bio", Prompt = "Add a short bio...", Order = 4)]
+    public string? Bio { get; set; }
+}
